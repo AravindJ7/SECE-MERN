c8de7f3515696315714042a21306dc44ae270e6b
Aravind <aravindj27012007@gmail.com>
2025-12-11 11:19:14 +0530

    Final

---
+++ MERN/SeceWinterIntern2025/src/functionalComponents/Navbar.jsx
@@ -0,0 +1,17 @@
+
+function Navbar(){
+    return(
+        <>
+            <nav style={{backgroundColor:"black",color:"aqua"}}>
+                <ul className="ul">
+                    <li className="li">Home</li>
+                    <li className="li">About</li>
+                    <li className="li">Contact</li>
+                </ul>
+
+            </nav>
+            <p>The above content is Navbar </p>
+        </>
+    );
+}
+export default Navbar;
